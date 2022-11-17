e363c8180a8f012b75bc7868e60b097b378ffc48
cleitonprof <cleitonprof@users.noreply.github.com>
2022-11-17 07:05:50 -0300

    Versão para a aula de 17/11/2022

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,27 +1,12 @@
 import './CampoTexto.css'
 
-/*
-const CampoTexto = (props) => {
-    console.log(props)
-    return(
-        <div>
-            <label>
-                {'Nome'}
-            </label>
-            <input placeholder='Digite o seu nome...'></input>
-        </div>
-    )
-}
-export default CampoTexto
-
-*/
-
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` 
 
     const aoDigitado = (evento) => {
         props.aoAlterado(evento.target.value)
+        //console.log(evento.target.value)
     }
 
     return (
